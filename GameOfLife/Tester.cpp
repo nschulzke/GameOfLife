d8debe6cb847b77c027e798017729fa7c8f82f2b
Nathan Schulzke <nschulzke@gmail.com>
2017-02-21 20:17:00 -0700

    Remove time testing frame in preparation for file I/O.

--- GameOfLife/Tester.cpp
+++ GameOfLife/Tester.cpp
@@ -7,32 +7,21 @@ const int MICROS_PER_SECOND = 1000000;
 
 int main()
 {
-	int rows = 200;
-	int cols = 200;
+	int rows = 20;
+	int cols = 20;
 
 	Seed seed = Seed::glider(rows, cols);
 
 	Life life(seed);
 
-	clock_t start;
-	clock_t end;
-	double ticks;
-	double time;
-	double average;
-
-	start = clock();
 	life.step(50);
 	life.overlay(seed);
 
 	while (life.hasLife())
+	{
+		std::cout << life << std::endl;
 		life.step();
-	end = clock();
-	ticks = end - start;
-	time = (ticks / (double)CLOCKS_PER_SEC) * MICROS_PER_SECOND;
-	average = time / life.steps();
-
-	std::cout << "Steps to death: " << life.steps() << std::endl;
-	std::cout << "Average time:   " << average << std::endl;
+	}
 
 	system("PAUSE");
 	return 0;
