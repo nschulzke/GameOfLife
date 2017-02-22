1adda21c182e72c0c89f8a54cd17aec0433fd921
Nathan Schulzke <nschulzke@gmail.com>
2017-02-21 20:51:08 -0700

    Update Tester to catch exception

--- GameOfLife/Tester.cpp
+++ GameOfLife/Tester.cpp
@@ -2,19 +2,27 @@
 #include <ctime>
 #include "Life.h"
 #include "Seed.h"
+#include "FileIOException.h"
 
 const int MICROS_PER_SECOND = 1000000;
 
 int main()
 {
-	Seed seed = Seed("test.txt");
-
-	Life life(seed);
-
-	for (int i = 0; i < 20; i++)
+	try
 	{
-		std::cout << life << std::endl;
-		life.step();
+		Seed seed = Seed("test.txt");
+
+		Life life(seed);
+
+		for (int i = 0; i < 20; i++)
+		{
+			std::cout << life << std::endl;
+			life.step();
+		}
+	}
+	catch (FileIOException e)
+	{
+		std::cout << e.toString() << std::endl;
 	}
 
 	system("PAUSE");
